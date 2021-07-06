d4a1a97adc4db3f566a045009e87ce133f1ab99b
alexandruai <andra2699@yahoo.com>
2021-07-06 17:29:54 +0300

    Simple Parts

--- Graphic User Interface/app/src/main/java/com/example/graphicuserinterface/AddSocketActivity.java
+++ Graphic User Interface/app/src/main/java/com/example/graphicuserinterface/AddSocketActivity.java
@@ -2,13 +2,62 @@ package com.example.graphicuserinterface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.graphicuserinterface.objects.Socket;
+
+import java.io.Serializable;
 
 public class AddSocketActivity extends AppCompatActivity {
+
+    EditText edIdNewSocket;
+    Button btnAddIdSocket;
+    Button btnBack;
+    public static final String ADD_Socket = "addSocket";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_socket);
+        edIdNewSocket = findViewById(R.id.etIdSocket);
+        btnAddIdSocket = findViewById(R.id.btnAdauga);
+        btnBack = findViewById(R.id.btnBack);
+        Intent intent = getIntent();
+
+        btnAddIdSocket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(edIdNewSocket.getText().toString().isEmpty()){
+                    edIdNewSocket.setError("Introduceti id priza fizica!");
+                } else {
+                    String id = edIdNewSocket.getText().toString();
+                    boolean[] state = {false, false, false, false};
+                    Socket socket = new Socket(id, state);
+
+                    //add restApi route catre server trimite noua priza id
+                    // routa si cu stari?
+
+                    intent.putExtra(ADD_Socket, (Serializable) socket);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+
+
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddSocketActivity.this, SelectSocketActivity.class);
+                startActivity(intent);
+            }
+        });
+        
     }
 }
--- Graphic User Interface/app/src/main/java/com/example/graphicuserinterface/SelectSocketActivity.java
+++ Graphic User Interface/app/src/main/java/com/example/graphicuserinterface/SelectSocketActivity.java
@@ -1,22 +1,77 @@
 package com.example.graphicuserinterface;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import com.example.graphicuserinterface.objects.Socket;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SelectSocketActivity extends AppCompatActivity {
 
+    public static final int ADD_Socket = 0;
+    public static final int REQUEST_CODE = 200;
+    public static final String Switch_State = "switch";
+    public static final int REQUEST_CODE_Switch = 300;
     private Button btnDelogare;
+    private Button btnAdaugaPrizaNoua;
+    private Button btnSchimbaStarePriza;
+    List<Socket> socketList = new ArrayList<>();
+    List<String> socketsStrings = new ArrayList<>();
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    int poz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_socket);
-
+        btnSchimbaStarePriza = findViewById(R.id.btnSwitchState);
+        btnAdaugaPrizaNoua = findViewById(R.id.btnPrizaNoua);
         btnDelogare = findViewById(R.id.btnDelogare);
+        listView = findViewById(R.id.listViewSockets);
+
+        for(Socket socket : socketList){
+            socketsStrings.add(socket.toString());
+        }
+        adapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                socketsStrings);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                poz = position;
+            }
+        });
+
+        btnSchimbaStarePriza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SwitchSocketStateActivity.class);
+                intent.putExtra(Switch_State, (Serializable) socketList.get(poz));
+                startActivityForResult(intent, REQUEST_CODE_Switch);
+            }
+        });
+
+        btnAdaugaPrizaNoua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AddSocketActivity.class);
+                startActivityForResult(intent, ADD_Socket);
+            }
+        });
 
         btnDelogare.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,4 +82,16 @@ public class SelectSocketActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            Socket socket = (Socket) data.getSerializableExtra(AddSocketActivity.ADD_Socket);
+            if(socket != null){
+                socketList.add(socket);
+                socketsStrings.add(socket.toString());
+                adapter.notifyDataSetChanged();
+            }
+        }
+    }
 }
